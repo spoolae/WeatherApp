e4c99dbe9009ec60db055f3a74880d3c181fee52
Spoolae <ohotnic147feed@gmail.com>
2023-02-24 13:02:05 +0100

    fix chart in dark theme

--- src/components/temperature.chart/TemperatureChart.jsx
+++ src/components/temperature.chart/TemperatureChart.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from "recharts";
-import { getChartData } from "../../constants/getChartData";
 
-export const TemperatureChart = ({ weather }) => {
+import "./TemperatureChartStyles.scss";
+import { getChartData } from "../../constants/getChartData";
+import { connect } from "react-redux";
+import { mapStateToProps } from "../../constants/mapStateToProps";
+
+const TemperatureChart = ({ weather, isDark }) => {
   const chartData = getChartData({ weather });
+  const [primaryColor, setPrimaryColor] = useState(null);
+
+  useEffect(() => {
+    setPrimaryColor(
+      isDark
+        ? getComputedStyle(document.documentElement).getPropertyValue(
+            "--light-grey-color"
+          )
+        : getComputedStyle(document.documentElement).getPropertyValue(
+            "--chart-color"
+          )
+    );
+    console.log(primaryColor);
+  }, [isDark]);
+
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="custom-tooltip">
+          <p className="time">{label}</p>
+          <p className="value">{`Temperature is ${payload[0].value}°C`}</p>
+        </div>
+      );
+    }
+
+    return null;
+  };
+
+  CustomTooltip.propTypes = {
+    active: PropTypes.bool,
+    payload: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string,
+        value: PropTypes.number,
+        unit: PropTypes.string,
+      })
+    ),
+    label: PropTypes.string,
+  };
 
   return (
     <ResponsiveContainer width="100%" height="60%">
@@ -12,12 +55,12 @@ export const TemperatureChart = ({ weather }) => {
         <Line
           type="natural"
           dataKey="t"
-          stroke="#5a81a3"
+          stroke={primaryColor}
           strokeWidth={3}
           activeDot={{ strokeWidth: 0, r: 6 }}
           isAnimationActive={false}
         />
-        <Tooltip />
+        <Tooltip content={<CustomTooltip />} />
         <XAxis dataKey="time" hide />
       </LineChart>
     </ResponsiveContainer>
@@ -26,4 +69,8 @@ export const TemperatureChart = ({ weather }) => {
 
 TemperatureChart.propTypes = {
   weather: PropTypes.object,
+  isDark: PropTypes.bool,
 };
+
+const ConnectedTemperatureChart = connect(mapStateToProps)(TemperatureChart);
+export { ConnectedTemperatureChart as TemperatureChart };
